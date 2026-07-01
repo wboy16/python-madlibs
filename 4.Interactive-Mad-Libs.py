90fb73e35700f20be7cf8ac6a132d5b0079dcc17
Mongreezy <gamefreak148d@gmail.com>
2026-07-01 12:10:53 +0200

    feat: interactive mad libs with input comments and variable reuse

---
+++ 4.Interactive-Mad-Libs.py
@@ -0,0 +1,19 @@
+# 4. Interactive Mad Libs
+
+# I'm collecting an adjective
+adjective = input("Enter an adjective: ")
+
+# I'm collecting a noun
+noun = input("Enter a noun: ")
+
+# I'm collecting a verb
+verb = input("Enter a verb: ")
+
+# I'm collecting the name of a place
+place = input("Enter a place: ")
+
+# I'm collecting the name of a famous person
+famous_person = input("Enter a famous person: ")
+
+print("Once upon a time, a " + adjective + " " + noun + " decided to " + verb + " all the way to the " + place +
+      ". And the name of this person was " + famous_person + ". And everyone cheered for " + famous_person + ".")
